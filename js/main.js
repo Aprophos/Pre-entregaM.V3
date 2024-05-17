ade45fcd08ca461ad74e2b944ed8d54b7b4bc69e
Aprophos <aliasquintoraul@gmail.com>
2024-05-16 23:45:22 -0400

    Sass y seo y map agregado

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.header-menu-list');
-// let menuItem = document.querySelectorAll('.header-menu-list-item');
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
-
-
-// menuItem.forEach (function(menuItem) {
-//   menuItem.addEventListener('click',function(){
-//           menuBtn.classList.toggle('active');
-//           menu.classList.toggle('active');
-//   })
-// })
-
-
